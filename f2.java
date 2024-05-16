b6293cbe46b0d0373caa5f3eb37c4a1039cba057
vaishu16003 <vaishalikaler16@gmail.com>
2024-05-16 20:18:40 +0530

    modified

--- f2.java
+++ f2.java
@@ -1,9 +1,21 @@
-fsdfsdfsdfsdfs
-dsfdfsdf
-sdfgdf
-  g
-  fdgdf
-  gdf
-  gdf
-  g
-  fdgsd
+import java.lang.*;
+public class file1
+{ 
+int a = 1600;
+int b= 2000;
+public void add()
+{
+System.out.println("sum is :" a+b);
+ 
+}
+
+public static void main(String args[])
+{
+System.out.println("hello");
+System.out.println("wipro");
+
+#modified by vaishu_branch
+file1 f1 = new file1();
+f1.add();
+}
+}
